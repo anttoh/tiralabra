aaee80f0c691383cb6012b45eff903aadd509a6f
Antto Heikura <antto.heikura@gmail.com>
2021-08-04 13:09:59 +0300

    lisää testi testaamaan pulman tulostamista

--- koodi/testit/test_pulma.py
+++ koodi/testit/test_pulma.py
@@ -35,3 +35,10 @@ def test_alas_toimii():
     pulma = pulma_testaukseen()
     assert pulma.alas()
     assert not pulma.alas()
+
+
+def test_tulosta_tulostaa_pulman_ruudukkona(capsys):
+    pulma_testaukseen().tulosta()
+    tulostettu_ruudukko = capsys.readouterr().out
+    toivottu_tulostus = "\n|1|2|3|\n|4|0|5|\n|6|7|8|\n"
+    assert tulostettu_ruudukko == toivottu_tulostus
